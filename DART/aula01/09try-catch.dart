3d957e02b49d96f9e4e40570ac486a1284233c4c
ralflima <ralf.lima@gmail.com>
2023-07-17 21:46:21 -0300

    Primeira aula de DART

--- DART/aula01/09try-catch.dart
+++ DART/aula01/09try-catch.dart
@@ -2,10 +2,12 @@ void main(){
 
   // Tentativa
   try{
-    int? numero = int.parse("a2");
+    int numero = int.parse("a2");
     print("O número informadoe é $numero");
   }catch(erro){
     print("Falha ao executar: $erro");
   }
 
 }
+
+
