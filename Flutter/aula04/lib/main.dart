3243bbd4d2d9513992a0e0c112371478abba6833
ralflima <ralf.lima@gmail.com>
2023-08-14 20:01:06 -0300

    Quarta aula de Flutter

--- Flutter/aula04/lib/main.dart
+++ Flutter/aula04/lib/main.dart
@@ -29,25 +29,52 @@ class Pagina extends StatefulWidget {
 }
 
 class ConteudoPagina extends State {
+  bool ativo = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Componentes de formulário"),
+      ),
+      //body: const ElementoStatefulWidget());
+      body: Switch(
+        value: ativo,
+        activeColor: Colors.green,
+        onChanged: (valor) {
+          setState(() {
+            ativo = valor;
+          });
+        },
+      ),
+    );
+  }
+}
+
+class ElementoStatefulWidget extends StatefulWidget {
+  const ElementoStatefulWidget({super.key});
+
+  @override
+  State<StatefulWidget> createState() {
+    return DropdownButtonState();
+  }
+}
+
+class DropdownButtonState extends State {
   // Lista contendo nomes
   List<String> nomes = ["Selecione um nome", "Ralf", "Isabella", "Rebeca"];
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: const Text("Componentes de formulário"),
-        ),
-        body: DropdownButton<String>(
-          value: nomes.first,
-          items: nomes.map(
-            (n) {
-              return DropdownMenuItem<String>(value: n, child: Text(n));
-            },
-          ).toList(),
-          onChanged: (nome) {
-            print("A opção escolhida é: $nome");
-          },
-        ));
+    return DropdownButton<String>(
+      items: nomes.map(
+        (n) {
+          return DropdownMenuItem<String>(value: n, child: Text(n));
+        },
+      ).toList(),
+      onChanged: (nome) {
+        print("A opção escolhida é: $nome");
+      },
+    );
   }
 }
