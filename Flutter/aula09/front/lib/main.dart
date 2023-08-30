314050524fc629f2d0a2d85b11c955434ea1314c
ralflima <ralf.lima@gmail.com>
2023-08-30 18:50:28 -0300

    Nona aula de Flutter

---
+++ Flutter/aula09/front/lib/main.dart
@@ -0,0 +1,62 @@
+// Importação do material
+import 'package:flutter/material.dart';
+
+// Importação do http
+import 'package:http/http.dart' as http;
+
+// Importação do convert
+import 'dart:convert';
+
+class Produto {
+  int? codigo;
+  String? nome;
+  String? imagem;
+}
+
+// Inicialização
+void main() {
+  runApp(const MyApp());
+}
+
+// Stateless
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.blueGrey,
+        ),
+      ),
+      home: const Pagina(),
+    );
+  }
+}
+
+// StatefulWidget
+class Pagina extends StatefulWidget {
+  const Pagina({super.key});
+
+  @override
+  State<StatefulWidget> createState() {
+    return ConteudoPagina();
+  }
+}
+
+// State
+class ConteudoPagina extends State {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("9ª aula de Flutter"),
+      ),
+      body: const Text(
+        "Hello World",
+        textDirection: TextDirection.ltr,
+      ),
+    );
+  }
+}
