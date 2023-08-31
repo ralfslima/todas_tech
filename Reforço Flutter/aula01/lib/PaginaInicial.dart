1302ba9175ac9f1a930b426eba0c4c78ea3c98f8
ralflima <ralf.lima@gmail.com>
2023-08-31 19:23:57 -0300

    Aula de Reforço - 01

---
+++ Reforço Flutter/aula01/lib/PaginaInicial.dart
@@ -0,0 +1,21 @@
+// Pacote Material
+import 'package:flutter/material.dart';
+
+// StatefulWidget
+class PaginaInicial extends StatefulWidget {
+  const PaginaInicial({super.key});
+
+  @override
+  State<StatefulWidget> createState() {
+    return PaginaInicialState();
+  }
+}
+
+// State
+class PaginaInicialState extends State {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
