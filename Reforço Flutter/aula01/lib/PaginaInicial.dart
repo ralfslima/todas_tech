a5775a247bd3635636e6380e5be54bb356a64f6a
ralflima <ralf.lima@gmail.com>
2023-08-31 20:19:11 -0300

    Aula de Reforço - 01

--- Reforço Flutter/aula01/lib/PaginaInicial.dart
+++ Reforço Flutter/aula01/lib/PaginaInicial.dart
@@ -22,40 +22,46 @@ class PaginaInicialState extends State {
           gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
               crossAxisCount: 2),
           children: [
-            // Container
-            Container(
-              decoration: BoxDecoration(
-                border: Border.all(color: Colors.black),
-                borderRadius: BorderRadius.circular(10),
-              ),
-              width: 500,
-              // Padding
-              child: Padding(
-                padding: EdgeInsets.all(10),
-                child: // COLUNA
-                    Column(
-                  children: [
-                    // IMAGEM
-                    Image.network(
-                        "https://media.staticontent.com/media/pictures/04641818-297a-4f28-b635-b15e2fb31087"),
-                    // TÍTULO
-                    const Text("Curitiba"),
-
-                    // LINHA CONTENDO OS BOTÕES
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+            Material(
+              child: InkWell(
+                onTap: () {},
+                child: // Container
+                    Container(
+                  decoration: BoxDecoration(
+                    border: Border.all(color: Colors.black),
+                    borderRadius: BorderRadius.circular(10),
+                    color: Colors.amber,
+                  ),
+                  width: 500,
+                  // Padding
+                  child: Padding(
+                    padding: EdgeInsets.all(10),
+                    child: // COLUNA
+                        Column(
                       children: [
-                        ElevatedButton(
-                          onPressed: () {},
-                          child: const Text("Pontos turísticos"),
-                        ),
-                        ElevatedButton(
-                          onPressed: () {},
-                          child: const Text("Mapa"),
-                        ),
+                        // IMAGEM
+                        Image.network(
+                            "https://media.staticontent.com/media/pictures/04641818-297a-4f28-b635-b15e2fb31087"),
+                        // TÍTULO
+                        const Text("Curitiba"),
+
+                        // LINHA CONTENDO OS BOTÕES
+                        // Row(
+                        //   mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        //   children: [
+                        //     ElevatedButton(
+                        //       onPressed: () {},
+                        //       child: const Text("Pontos turísticos"),
+                        //     ),
+                        //     ElevatedButton(
+                        //       onPressed: () {},
+                        //       child: const Text("Mapa"),
+                        //     ),
+                        //   ],
+                        // )
                       ],
-                    )
-                  ],
+                    ),
+                  ),
                 ),
               ),
             ),
