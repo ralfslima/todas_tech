50f4590a0c3fc3b41a122ae2bff01949f823d6a9
ralflima <ralf.lima@gmail.com>
2023-08-31 20:06:31 -0300

    Aula de Reforço - 01

--- Reforço Flutter/aula01/lib/PaginaInicial.dart
+++ Reforço Flutter/aula01/lib/PaginaInicial.dart
@@ -15,37 +15,50 @@ class PaginaInicial extends StatefulWidget {
 class PaginaInicialState extends State {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        border: Border.all(color: Colors.black),
-        borderRadius: BorderRadius.circular(10),
-      ),
-      width: 500,
-      child: Padding(
-        padding: EdgeInsets.all(10),
-        child: // COLUNA
-            Column(
+    return Center(
+      child: SizedBox(
+        width: 500,
+        child: GridView(
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2),
           children: [
-            // IMAGEM
-            Image.network(
-                "https://media.staticontent.com/media/pictures/04641818-297a-4f28-b635-b15e2fb31087"),
-            // TÍTULO
-            const Text("Curitiba"),
+            // Container
+            Container(
+              decoration: BoxDecoration(
+                border: Border.all(color: Colors.black),
+                borderRadius: BorderRadius.circular(10),
+              ),
+              width: 500,
+              // Padding
+              child: Padding(
+                padding: EdgeInsets.all(10),
+                child: // COLUNA
+                    Column(
+                  children: [
+                    // IMAGEM
+                    Image.network(
+                        "https://media.staticontent.com/media/pictures/04641818-297a-4f28-b635-b15e2fb31087"),
+                    // TÍTULO
+                    const Text("Curitiba"),
 
-            // LINHA CONTENDO OS BOTÕES
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                ElevatedButton(
-                  onPressed: () {},
-                  child: const Text("Pontos turísticos"),
+                    // LINHA CONTENDO OS BOTÕES
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        ElevatedButton(
+                          onPressed: () {},
+                          child: const Text("Pontos turísticos"),
+                        ),
+                        ElevatedButton(
+                          onPressed: () {},
+                          child: const Text("Mapa"),
+                        ),
+                      ],
+                    )
+                  ],
                 ),
-                ElevatedButton(
-                  onPressed: () {},
-                  child: const Text("Mapa"),
-                ),
-              ],
-            )
+              ),
+            ),
           ],
         ),
       ),
